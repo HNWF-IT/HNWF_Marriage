ef2f06f44868b87e7fa1be76399b3e863c811ca2
HNWF-IT <hnwf.it.work@gmail.com>
2025-06-21 06:07:06 +0500

    import bcrypt

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const { signUpSchema } = require('../schemas/user');
+const bcrypt = require('bcryptjs');
 
 // Create a new user
 router.post('/create', async (req, res) => {
